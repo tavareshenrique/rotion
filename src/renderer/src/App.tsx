6fd6f2e09aa8b18d2a07d10487ac6b438842fa36
Henrique Tavares <ihenrits@gmail.com>
2023-07-27 10:02:44 -0300

    feat: app main

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,19 @@
+import { Header, Sidebar } from './components';
+
 import './styles/global.css';
 
 export function App() {
   return (
-    <div className="h-screen w-screen bg-rotion-900 text-rotion-100">
-      <h1>Ola</h1>
+    <div className="h-screen w-screen text-rotion-100 flex">
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+
+        <main className="flex-1 flex- items-center justify-center text-rotion-400">
+          Selecione ou crie um documento
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/renderer/src/components/index.ts
@@ -0,0 +1,4 @@
+export { Header } from './Header';
+export { SearchBar } from './SearchBar';
+export { Sidebar } from './Sidebar';
+export { ToC } from './ToC';
