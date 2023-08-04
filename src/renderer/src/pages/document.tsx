49ac6d721a1ef18aa53ee756e294da4b0f420eeb
Henrique Tavares <ihenrits@gmail.com>
2023-08-04 07:20:39 -0300

    document page

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,25 @@
-import { Link } from 'react-router-dom';
+import { ToC } from '../components';
 
 export function Document() {
   return (
-    <main className="flex items-center justify-center flex-1 text-rotion-400">
-      Document
-      <Link to="/">Acessar o Blank</Link>
+    <main className="flex flex-1 gap-8 px-10 py-12">
+      <aside className="sticky top-0 hidden lg:block">
+        <span className="text-xs font-semibold text-rotion-300">
+          TABLE OF CONTENTS
+        </span>
+
+        <ToC.Root>
+          <ToC.Link>Back-end</ToC.Link>
+          <ToC.Section>
+            <ToC.Link>Banco de dados</ToC.Link>
+            <ToC.Link>Autenticação</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+
+      <section className="flex flex-col items-center flex-1">
+        Hello World
+      </section>
     </main>
   );
 }
