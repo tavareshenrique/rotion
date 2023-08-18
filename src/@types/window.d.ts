095aaeeae174d1f62417b9f39da60a789695b8f4
Henrique Tavares <ihenrits@gmail.com>
2023-08-18 08:26:11 -0300

    work with ipc and react query

--- src/@types/window.d.ts
+++ src/@types/window.d.ts
@@ -1,8 +1,10 @@
 import { ElectronAPI } from '@electron-toolkit/preload';
 
+import { api } from '../preload';
+
 declare global {
   export interface Window {
     electron: ElectronAPI;
-    api: unknown;
+    api: typeof api;
   }
 }
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,8 +1,12 @@
-import { contextBridge } from 'electron';
+import { contextBridge, ipcRenderer } from 'electron';
 import { electronAPI } from '@electron-toolkit/preload';
 
 // Custom APIs for renderer
-const api = {};
+export const api = {
+  fetchDocuments(): Promise<Array<{ id: string; title: string }>> {
+    return ipcRenderer.invoke('fetch-documents');
+  },
+};
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
