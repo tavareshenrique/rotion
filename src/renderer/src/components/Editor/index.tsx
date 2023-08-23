e1cd11b6e75daeb65a82465f051bde0d9e4f6970
Henrique Tavares <ihenrits@gmail.com>
2023-08-23 11:04:24 -0300

    save document

--- src/renderer/src/components/Editor/index.tsx
+++ src/renderer/src/components/Editor/index.tsx
@@ -6,11 +6,17 @@ import Highlight from '@tiptap/extension-highlight';
 import Typography from '@tiptap/extension-typography';
 import Placeholder from '@tiptap/extension-placeholder';
 
+export type TOnContentUpdatedParams = {
+  title: string;
+  content: string;
+};
+
 interface IEditorProps {
   content: string;
+  onContentUpdated: (params: TOnContentUpdatedParams) => void;
 }
 
-export function Editor({ content }: IEditorProps) {
+export function Editor({ content, onContentUpdated }: IEditorProps) {
   const editor = useEditor({
     extensions: [
       Document.extend({
@@ -27,6 +33,18 @@ export function Editor({ content }: IEditorProps) {
           'before:content-[attr(data-placeholder)] before:text-gray-500 before:h-0 before:float-left before:pointer-events-none',
       }),
     ],
+    onUpdate: ({ editor }) => {
+      const contentRegex = /(<h1>(?<tittle>.+)<\/h1>(?<content>.+)?)/;
+      const parsedContent = editor.getHTML().match(contentRegex)?.groups;
+
+      const title = parsedContent?.tittle ?? 'Untitle';
+      const content = parsedContent?.content ?? '';
+
+      onContentUpdated({
+        title,
+        content,
+      });
+    },
     content,
     autofocus: 'end',
     editorProps: {
--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,16 +1,47 @@
-import { useParams } from 'react-router-dom';
-import { Editor, ToC } from '../components';
-import { useQuery } from '@tanstack/react-query';
 import { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+import { Editor, ToC } from '../components';
+
+import { TOnContentUpdatedParams } from '../components/Editor';
+import { IDocument } from '~/src/shared/types/ipc';
 
 export function Document() {
   const { id } = useParams<{ id: string }>();
+  const queryClient = useQueryClient();
 
   const { data, isFetching } = useQuery(['document', id], async () => {
     const response = await window.api.fetchDocument({ id: id! });
 
     return response.data;
   });
+
+  const { mutateAsync: saveDocument } = useMutation(
+    async ({ title, content }: TOnContentUpdatedParams) => {
+      await window.api.saveDocument({
+        id: id!,
+        title,
+        content,
+      });
+    },
+    {
+      onSuccess: (_, { title }) => {
+        queryClient.setQueryData<IDocument[]>(['documents'], (document) => {
+          return document?.map((doc) => {
+            if (doc.id === id) {
+              return {
+                ...doc,
+                title,
+              };
+            }
+
+            return doc;
+          });
+        });
+      },
+    },
+  );
 
   const inititalContent = useMemo(() => {
     if (data) {
@@ -19,6 +50,13 @@ export function Document() {
 
     return '';
   }, [data]);
+
+  function handleEditorContentUpdated({
+    title,
+    content,
+  }: TOnContentUpdatedParams) {
+    saveDocument({ title, content });
+  }
 
   return (
     <main className="flex flex-1 gap-8 px-10 py-12">
@@ -37,7 +75,12 @@ export function Document() {
       </aside>
 
       <section className="flex flex-col items-center flex-1">
-        {!isFetching && data && <Editor content={inititalContent} />}
+        {!isFetching && data && (
+          <Editor
+            content={inititalContent}
+            onContentUpdated={handleEditorContentUpdated}
+          />
+        )}
       </section>
     </main>
   );
