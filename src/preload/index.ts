f122112788545162bb5bdd8d3d63886d0a96f26b
Henrique Tavares <ihenrits@gmail.com>
2023-08-23 10:03:55 -0300

    CRUD structure with IPC

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -2,12 +2,35 @@ import { contextBridge, ipcRenderer } from 'electron';
 import { electronAPI } from '@electron-toolkit/preload';
 
 import { IPC } from '@shared/constants/ipc';
-import { FetchAllDocumentsResponse } from '@shared/types/ipc';
+import {
+  CreateDocumentResponse,
+  DeleteDocumentRequest,
+  FetchAllDocumentsResponse,
+  FetchDocumentRequest,
+  FetchDocumentResponse,
+  SaveDocumentRequest,
+} from '@shared/types/ipc';
 
 // Custom APIs for renderer
 export const api = {
   fetchDocuments(): Promise<FetchAllDocumentsResponse> {
     return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH_ALL);
+  },
+
+  fetchDocument(req: FetchDocumentRequest): Promise<FetchDocumentResponse> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH, req);
+  },
+
+  createDocument(): Promise<CreateDocumentResponse> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.CREATE);
+  },
+
+  saveDocument(req: SaveDocumentRequest): Promise<void> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.SAVE, req);
+  },
+
+  deleteDocument(req: DeleteDocumentRequest): Promise<void> {
+    return ipcRenderer.invoke(IPC.DOCUMENTS.DELETE, req);
   },
 };
 
--- src/shared/types/ipc.ts
+++ src/shared/types/ipc.ts
@@ -1,17 +1,33 @@
-export interface IDocument {
+export interface Document {
   id: string;
   title: string;
-  content: string;
+  content?: string;
 }
 
 /**
  * Request
  */
+export type SaveDocumentRequest = Document;
+
+export interface FetchDocumentRequest {
+  id: string;
+}
+
+export interface DeleteDocumentRequest {
+  id: string;
+}
 
 /**
  * Response
  */
-
 export interface FetchAllDocumentsResponse {
-  data: IDocument[];
+  data: Document[];
+}
+
+export interface FetchDocumentResponse {
+  data: Document;
+}
+
+export interface CreateDocumentResponse {
+  data: Document;
 }
